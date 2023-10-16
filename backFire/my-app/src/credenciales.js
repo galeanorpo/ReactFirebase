523d258d1ab4830c4ee8271e36ce3a3c22e78c89
galeanorpo <juanpablogaleano101@gmail.com>
2023-10-16 18:34:36 -0500

    first commit

---
+++ backFire/my-app/src/credenciales.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR6l8H-bG33xsh5-Jq-HUvlS9-jmwrYq4",
+  authDomain: "backreact-3dfa7.firebaseapp.com",
+  projectId: "backreact-3dfa7",
+  storageBucket: "backreact-3dfa7.appspot.com",
+  messagingSenderId: "1079637430848",
+  appId: "1:1079637430848:web:2e833ea66c3800c4be45a5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
